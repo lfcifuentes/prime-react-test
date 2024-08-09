006a4503e3eccac06244ef2c12aea478900dfd76
Luis Fernando Cifuentes <lfcifuentes28@gmail.com>
2024-08-08 23:12:10 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, fugiat eius! Expedita nostrum perferendis libero omnis, soluta ab quos? Totam ut sunt ab error exercitationem iusto expedita quisquam nisi corporis.
+      </section>
+    </div>
+  );
+}
+
+export default App;
